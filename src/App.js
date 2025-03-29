cea602d63ef3d6bf4bbeb77ff103c46557d0815a
eraysaglam <psmeray@outlook.com>
2025-03-29 17:42:05 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
+import LoginPage from './components/loginpage';
 
 function App() {
-  return (
-    <div style={{ fontFamily: 'sans-serif', padding: 20 }}>
-      <h1>Welcome to ErX Driver UI</h1>
-      <p>This is a working React setup.</p>
-    </div>
-  );
+  return <LoginPage />;
 }
 
 export default App;
