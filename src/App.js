6d3cbd788d0ddf3a58b6655c517a99e65883627d
eraysaglam <psmeray@outlook.com>
2025-03-29 17:48:47 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import LoginPage from './components/loginpage';
 
 function App() {
-  return <LoginPage />;
+  return (
+    <LoginPage />
+  );
 }
 
 export default App;
