36051bbe8f8713f38af1640cd592793689300c35
eraysaglam <psmeray@outlook.com>
2025-03-29 19:08:13 -0400

    Update MapBackground.js

--- src/src/MapBackground.js
+++ src/src/MapBackground.js
@@ -3,11 +3,11 @@ import React, { useEffect } from 'react';
 const MapBackground = () => {
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY`; // <-- BURAYA KENDİ KEY'İNİ YAZ
+    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyCV0Ajdezk2kzVdyBEFA-RgNsrxvwqYKMo`;
     script.async = true;
     script.onload = () => {
       new window.google.maps.Map(document.getElementById("map"), {
-        center: { lat: 40.7128, lng: -74.0060 }, // Örn: New York koordinatları
+        center: { lat: 40.7128, lng: -74.0060 }, // New York koordinatları
         zoom: 12,
         disableDefaultUI: true,
       });
