dd22bc45093b553ea69013c40d2bf424fb4f550b
eraysaglam <psmeray@outlook.com>
2025-03-29 19:39:31 -0400

    Update loginpage.js

--- src/src/loginpage.js
+++ src/src/loginpage.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import MapBackground from './MapBackground'; // Import the MapBackground component
 
 const LoginPage = () => {
   return (
     <div style={styles.container}>
-      <MapBackground /> {/* Call the map background here */}
-      <div style={styles.overlay}></div>
       <div style={styles.card}>
-        <h1>ErX Driver</h1>
-        <h3>Welcome back, please login</h3>
+        <h1 style={styles.title}>ErX Driver</h1>
+        <p style={styles.subtitle}>Welcome back, please login</p>
         <input style={styles.input} type="text" placeholder="Kullanıcı Adı" />
         <input style={styles.input} type="password" placeholder="Şifre" />
         <button style={styles.button}>Giriş Yap</button>
@@ -20,30 +17,30 @@ const LoginPage = () => {
 const styles = {
   container: {
     height: '100vh',
-    backgroundColor: '#e0e0e0',
-    position: 'relative',
+    backgroundColor: '#f0f0f0', // Gri arka plan
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    fontFamily: 'sans-serif',
-  },
-  overlay: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    zIndex: 1,
+    fontFamily: 'Arial, sans-serif',
   },
   card: {
-    backgroundColor: 'rgba(255, 255, 255, 0.9)',
-    padding: '40px',
+    backgroundColor: 'white',
+    padding: '30px',
     borderRadius: '10px',
-    boxShadow: '0 5px 15px rgba(0, 0, 0, 0.3)',
-    zIndex: 2,
+    boxShadow: '0px 5px 15px rgba(0, 0, 0, 0.1)',
     minWidth: '300px',
     textAlign: 'center',
+  },
+  title: {
+    fontSize: '24px',
+    fontWeight: 'bold',
+    marginBottom: '10px',
+    color: '#007bff',
+  },
+  subtitle: {
+    fontSize: '16px',
+    color: '#777',
+    marginBottom: '20px',
   },
   input: {
     width: '100%',
