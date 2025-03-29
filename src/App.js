84fa1c58fbc6c301fe19af8bc86b5b33a308f364
eraysaglam <psmeray@outlook.com>
2025-03-29 16:29:07 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: 20 }}>
+      <h1>Welcome to ErX Driver UI</h1>
+      <p>This is a working React setup.</p>
+    </div>
+  );
+}
+
+export default App;
